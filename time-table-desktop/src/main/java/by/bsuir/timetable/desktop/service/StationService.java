639af1efa427104e3d7aa3c08bf1b3cc90b01d73
Alexey Suslov <alexei.suslov@gmail.com>
2016-04-05 22:40:32 +0300

    security in progress

--- time-table-desktop/src/main/java/by/bsuir/timetable/desktop/service/StationService.java
+++ time-table-desktop/src/main/java/by/bsuir/timetable/desktop/service/StationService.java
@@ -1,8 +1,12 @@
 package by.bsuir.timetable.desktop.service;
 
 import by.bsuir.timetable.desktop.dto.StationDto;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +23,19 @@ public class StationService {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<StationDto[]> responseEntity = restTemplate.getForEntity(host + "/station?name=" + name, StationDto[].class);
         StationDto[] stations = responseEntity.getBody();
+
+
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                (HttpComponentsClientHttpRequestFactory) restTemplate.getRequestFactory();
+        DefaultHttpClient httpClient =
+                (DefaultHttpClient) requestFactory.getHttpClient();
+        httpClient.getCredentialsProvider().setCredentials(
+                new AuthScope(host, 8888, AuthScope.ANY_REALM),
+                new UsernamePasswordCredentials("name", "pass"));
+
+
+
+
         return Arrays.asList(stations);
     }
 
