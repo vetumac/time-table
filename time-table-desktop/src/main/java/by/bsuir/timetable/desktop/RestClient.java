d77987fe4e4b72376373e1f6b48021e0bc88b4c1
Alexey Suslov <alexei.suslov@gmail.com>
2016-04-11 23:38:53 +0300

    spring security in desktop work

--- time-table-desktop/src/main/java/by/bsuir/timetable/desktop/RestClient.java
+++ time-table-desktop/src/main/java/by/bsuir/timetable/desktop/RestClient.java
@@ -1,140 +1,59 @@
 package by.bsuir.timetable.desktop;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import java.net.URI;
-
+@Component
 public class RestClient {
-    private String host = "localhost";
-    private String port = "8080";
-    private String applicationPath;
-    private String apiPath = "api";
-    private String loginPath = "j_spring_security_check";
+
+    @Value("${host}")
+    private String host;
+
+    @Value("${port}")
+    private String port;
+
+    private String loginPath = "login";
     private String logoutPath = "logout";
-    private final String usernameInputFieldName = "j_username";
-    private final String passwordInputFieldName = "j_password";
+    private final String usernameInputFieldName = "username";
+    private final String passwordInputFieldName = "password";
     private final StatefullRestTemplate template = new StatefullRestTemplate();
 
-    /**
-     * This method logs into a service by doing an standard http using the configuration in this class.
-     *
-     * @param username the username to log into the application with
-     * @param password the password to log into the application with
-     * @return the url that the login redirects to
-     */
-    public String login(String username, String password) {
+    public boolean login(String username, String password) {
         MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
         form.add(usernameInputFieldName, username);
         form.add(passwordInputFieldName, password);
-        URI location = this.template.postForLocation(loginUrl(), form);
-        return location.toString();
+        return !template.postForLocation(loginUrl(), form).toString().matches("(.*)error(.*)");
     }
 
-    /**
-     * Logout by doing an http get on the logout url
-     *
-     * @return result of the get as ResponseEntity
-     */
     public ResponseEntity<String> logout() {
         return this.template.getForEntity(logoutUrl(), String.class);
-    }
-
-    public String applicationUrl(String relativePath) {
-        return applicationUrl() + "/" + checkNotNull(relativePath);
-    }
-
-    public String apiUrl(String relativePath) {
-        return applicationUrl(apiPath + "/" + checkNotNull(relativePath));
     }
 
     public StatefullRestTemplate template() {
         return template;
     }
 
+    public String apiUrl(String relativePath) {
+        return serverUrl() + "/" + checkNotNull(relativePath);
+    }
+
     public String serverUrl() {
         return "http://" + host + ":" + port;
     }
 
-    public String applicationUrl() {
-        return serverUrl() + "/" +.nullToEmpty(applicationPath);
-    }
-
     public String loginUrl() {
-        return applicationUrl(loginPath);
+        return serverUrl() + "/" + loginPath;
     }
 
     public String logoutUrl() {
-        return applicationUrl(logoutPath);
+        return serverUrl() + "/" + logoutPath;
     }
 
-    public String apiUrl() {
-        return applicationUrl(apiPath);
-    }
-
-    public void setLogoutPath(String logoutPath) {
-        this.logoutPath = logoutPath;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public String getPort() {
-        return port;
-    }
-
-    public void setPort(String port) {
-        this.port = port;
-    }
-
-    public String getApplicationPath() {
-        return applicationPath;
-    }
-
-    public void setApplicationPath(String contextPath) {
-        this.applicationPath = contextPath;
-    }
-
-    public String getApiPath() {
-        return apiPath;
-    }
-
-    public void setApiPath(String apiPath) {
-        this.apiPath = apiPath;
-    }
-
-    public String getLoginPath() {
-        return loginPath;
-    }
-
-    public void setLoginPath(String loginPath) {
-        this.loginPath = loginPath;
-    }
-
-    public String getLogoutPath() {
-        return logoutPath;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("RestClient [\n serverUrl()=");
-        builder.append(serverUrl());
-        builder.append(", \n applicationUrl()=");
-        builder.append(applicationUrl());
-        builder.append(", \n loginUrl()=");
-        builder.append(loginUrl());
-        builder.append(", \n logoutUrl()=");
-        builder.append(logoutUrl());
-        builder.append(", \n apiUrl()=");
-        builder.append(apiUrl());
-        builder.append("\n]");
-        return builder.toString();
+    private String checkNotNull(String str) {
+        if (str != null) return str;
+        else throw new IllegalArgumentException("Must e not null.");
     }
 }
--- time-table-desktop/src/main/java/by/bsuir/timetable/desktop/controller/LoginController.java
+++ time-table-desktop/src/main/java/by/bsuir/timetable/desktop/controller/LoginController.java
@@ -1,8 +1,10 @@
 package by.bsuir.timetable.desktop.controller;
 
+import by.bsuir.timetable.desktop.service.LoginService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,11 +19,20 @@ public class LoginController {
     @FXML
     private Button loginButton;
 
+    @Autowired
+    private LoginService loginService;
+
     @FXML
     void initialize() {
-        loginButton.setOnAction(event -> {
-            loginButton.getScene().getWindow().hide();
+        loginButton.setOnAction(event ->
+        {
+            //loginButton.getScene().getWindow().hide();
+            loginButton.setText(String.valueOf(loginService.login(loginTextField.getText(), passwordTextField.getText())));
+
         });
     }
+
+    ;
+
 
 }
